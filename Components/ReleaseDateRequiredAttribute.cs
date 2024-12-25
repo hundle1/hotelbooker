af05c2287c47f9e83b3966a1c4e3bc827a0e58ac
Nguyễn Mạnh Dũng <trexhundle@gmail.com>
2024-12-25 10:07:34 +0700

    updating room status with date-requirer

---
+++ Components/ReleaseDateRequiredAttribute.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using HotelBooker.Models;
+
+public class ReleaseDateRequiredAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var room = (Room)validationContext.ObjectInstance;
+
+        if (room.Status && value == null)
+        {
+            return new ValidationResult("ReleaseDate is required when the room is In Used.");
+        }
+        return ValidationResult.Success;
+    }
+}
--- Models/Room.cs
+++ Models/Room.cs
@@ -5,7 +5,7 @@ public class Room {
     public int Id { get; set; }
     public string? RoomName { get; set; }
     [DataType(DataType.Date)]
-
+    [ReleaseDateRequired]
     public bool Status { get; set; }
     public DateTime ReleaseDate { get; set; }
     public string? Services { get; set; }
