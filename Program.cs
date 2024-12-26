e2f7043d0b19812c8274ffbf58e77c7adaf1db65
Nguyễn Mạnh Dũng <trexhundle@gmail.com>
2024-12-26 21:20:10 +0700

    tao conflict

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using HotelBooker.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HotelBookerContext>(options =>
